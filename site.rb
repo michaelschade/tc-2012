fe1dfad4a99d18c9e2f0aaf91795c3835537e6d4
Michael Schade <michael@spearheaddev.com>
2012-09-03 07:29:06 -0700

    Get paid! Process one-time payments through Stripe.

--- site.rb
+++ site.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'stripe'
+
+Stripe.api_key = 'sk_0JzIVHOD3WeYgfMnsLGQy7vu9MoVh'
 
 get '/' do
   erb :index
+end
+
+post '/register' do
+  begin
+    Stripe::Charge.create(
+      :amount       => 4200,
+      :currency     => 'usd',
+      :card         => params[:stripeToken],
+      :description  => 'Conference pass for TE Emerge'
+    )
+  rescue Stripe::StripeError => error
+    redirect to('/')
+  end
+
+  erb :registered
 end
